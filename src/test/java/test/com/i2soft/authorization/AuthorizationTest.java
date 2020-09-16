1d165baff15c0ca62c0d7d6fe4af9cec864fcf63
ganl <ganl@info2soft.com>
2020-09-17 00:58:59 +0800

    [IM]: 资源授权相关方法测试通过

--- src/test/java/test/com/i2soft/authorization/AuthorizationTest.java
+++ src/test/java/test/com/i2soft/authorization/AuthorizationTest.java
@@ -6,6 +6,7 @@ import com.i2soft.http.I2Rs;
 import com.i2soft.http.I2softException;
 import com.i2soft.http.Response;
 import com.i2soft.util.Configuration;
+import com.i2soft.util.Json;
 import com.i2soft.util.StringMap;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -37,11 +38,11 @@ public class AuthorizationTest {
     }
 
     @Test
-    public void T1_tempFuncName() {
+    public void T1_ListAuthorizationUser() {
         try {
             Response r = auth.client.get(String.format(TestConfig.rapDataUrl, "1563")); // 获取请求数据
             StringMap args = new StringMap().putAll(Objects.requireNonNull(r.jsonToMap())); // 填充请求数据
-            Map rs = authorization.tempFuncName(args); // 发送请求
+            Map rs = authorization.ListAuthorizationUser(args); // 发送请求
             Assert.assertNotNull(rs); // 检查结果
         } catch (I2softException e) {
             e.printStackTrace();
@@ -52,7 +53,11 @@ public class AuthorizationTest {
     public void T2_getAuthorizationUserBind() {
         try {
             Response r = auth.client.get(String.format(TestConfig.rapDataUrl, "1564")); // 获取请求数据
-            StringMap args = new StringMap().putAll(Objects.requireNonNull(r.jsonToMap())); // 填充请求数据
+            Map aa = r.jsonToMap();
+            aa.put("user_uuid", "7AE5647A-5413-A7C3-449E-0CB18065E72B");
+            aa.put("type", 1);
+            aa.put("subtype", 0);
+            StringMap args = new StringMap().putAll(Objects.requireNonNull(aa)); // 填充请求数据
             Map rs = authorization.getAuthorizationUserBind(args); // 发送请求
             Assert.assertNotNull(rs); // 检查结果
         } catch (I2softException e) {
@@ -76,7 +81,10 @@ public class AuthorizationTest {
     public void T4_getAuthorizationResBind() {
         try {
             Response r = auth.client.get(String.format(TestConfig.rapDataUrl, "1666")); // 获取请求数据
-            StringMap args = new StringMap().putAll(Objects.requireNonNull(r.jsonToMap())); // 填充请求数据
+            Map aa = r.jsonToMap();
+            aa.put("res_uuid", "11111111-1111-1111-1111-111111111111");
+            aa.put("limit", 20);
+            StringMap args = new StringMap().putAll(Objects.requireNonNull(aa)); // 填充请求数据
             Map rs = authorization.getAuthorizationResBind(args); // 发送请求
             Assert.assertNotNull(rs); // 检查结果
         } catch (I2softException e) {
@@ -88,7 +96,17 @@ public class AuthorizationTest {
     public void T5_updateAuthorizationResBind() {
         try {
             Response r = auth.client.get(String.format(TestConfig.rapDataUrl, "1667")); // 获取请求数据
-            StringMap args = new StringMap().putAll(Objects.requireNonNull(r.jsonToMap())); // 填充请求数据
+            String rJson = "{\n" +
+                    "  \"res_uuid\": \"11111111-1111-1111-1111-111111111111\",\n" +
+                    "  \"user_list\": [\n" +
+                    "    {\n" +
+                    "      \"user_uuid\": \"719138DB-F25E-8D47-2D8D-86C2F99E71CB\",\n" +
+                    "      \"can_up\": 1,\n" +
+                    "      \"can_op\": 1\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}";
+            StringMap args = new StringMap().putAll(Objects.requireNonNull(Json.decode(rJson).map())); // 填充请求数据
             I2Rs.I2SmpRs rs = authorization.updateAuthorizationResBind(args); // 发送请求
             Assert.assertNotNull(rs); // 检查结果
         } catch (I2softException e) {
