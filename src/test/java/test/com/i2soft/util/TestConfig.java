8304284e3ad9fd3a406248411315b48f291bc200
ganl <ganl@info2soft.com>
2020-10-19 17:53:24 +0800

    [IM]: HA规则增加单个节点操作

--- src/test/java/test/com/i2soft/util/TestConfig.java
+++ src/test/java/test/com/i2soft/util/TestConfig.java
@@ -2,7 +2,7 @@ package test.com.i2soft.util;
 
 public final class TestConfig {
 
-    public static final String ip = "https://172.20.68.208:58086";
+    public static final String ip = "https://172.20.34.17:58086";
     public static final String user = "admin";
     public static final String pwd = "info1234";
     public static final String cachePath = "E:\\cache\\";
@@ -12,7 +12,8 @@ public final class TestConfig {
 
     public static final String rapDataUrl = "https://rap2api.info2soft.com/app/mock/data/%s?scope=request";
 
-    public static final String testUuid = "4D4965A3-54DE-69D8-3DBB-E85923C93396";
+    public static final String nodeUuid = "4D4965A3-54DE-69D8-3DBB-E85923C93396";
+    public static final String testUuid = "5D4965A3-54DE-69D8-3DBB-E85923C93396";
 
     private TestConfig() {
     }
