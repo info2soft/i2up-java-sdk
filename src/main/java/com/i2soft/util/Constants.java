9274a4728b177a99dfbb7cf1185f579ef86e02d9
ganl <ganl@info2soft.com>
2022-06-22 16:27:26 +0800

    [IM]: upgrade gradle 4.9

--- src/main/java/com/i2soft/util/Constants.java
+++ src/main/java/com/i2soft/util/Constants.java
@@ -1,6 +1,7 @@
 package com.i2soft.util;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * SDK相关配置常量
@@ -17,7 +18,7 @@ public final class Constants {
     /**
      * 所有都是UTF-8编码
      */
-    public static final Charset UTF_8 = Charset.forName("UTF-8");
+    public static final Charset UTF_8 = StandardCharsets.UTF_8;
     /**
      * 连接超时时间 单位秒(默认10s)
      */
