a761d3d4302739dbe93f7586db1673cfbd53e494
ganl <ganl@info2soft.com>
2022-06-22 14:59:11 +0800

    [IM]: rename test package to v20190805

---
+++ src/test/java/com/i2soft/i2up/v20190805/common/DataBaseBackupTest.java
@@ -0,0 +1,89 @@
+package com.i2soft.i2up.v20190805.common;
+
+import com.i2soft.common.Auth;
+import com.i2soft.common.v20190805.DataBaseBackup;
+import com.i2soft.http.I2Rs;
+import com.i2soft.http.I2softException;
+import com.i2soft.http.Response;
+import com.i2soft.i2up.v20190805.util.TestConfig;
+import com.i2soft.util.Configuration;
+import com.i2soft.util.StringMap;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.Objects;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DataBaseBackupTest {
+
+    private static Auth auth;
+    private static DataBaseBackup dataBaseBackup;
+
+    @BeforeClass
+    public static void setUp() {
+        if (dataBaseBackup != null) {
+            return;
+        }
+        try {
+            auth = Auth.token(TestConfig.ip, TestConfig.user, TestConfig.pwd, TestConfig.cachePath, new Configuration());
+        } catch (I2softException e) {
+            e.printStackTrace();
+        }
+        dataBaseBackup = new DataBaseBackup(auth);
+    }
+
+    @Test
+    public void T222_importConfig() {
+        try {
+            Response r = auth.client.get(String.format(TestConfig.rapDataUrl, "222")); // 获取请求数据
+            StringMap args = new StringMap().putAll(Objects.requireNonNull(r.jsonToMap())); // 填充请求数据
+            I2Rs.I2SmpRs rs = dataBaseBackup.importConfig(args); // 发送请求
+            Assert.assertNotNull(rs); // 检查结果
+        } catch (I2softException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void T223_exportConfig() {
+        try {
+            I2Rs.I2SmpRs rs = dataBaseBackup.exportConfig(); // 发送请求
+            Assert.assertNotNull(rs); // 检查结果
+        } catch (I2softException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void T224_listBackupHistory() {
+        try {
+            I2Rs.I2SmpRs rs = dataBaseBackup.listBackupHistory(); // 发送请求
+            Assert.assertNotNull(rs); // 检查结果
+        } catch (I2softException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void T225_backupConfig() {
+        try {
+            I2Rs.I2SmpRs rs = dataBaseBackup.backupConfig(); // 发送请求
+            Assert.assertNotNull(rs); // 检查结果
+        } catch (I2softException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void T226_describeBackupConfig() {
+        try {
+            I2Rs.I2SmpRs rs = dataBaseBackup.describeBackupConfig(); // 发送请求
+            Assert.assertNotNull(rs); // 检查结果
+        } catch (I2softException e) {
+            e.printStackTrace();
+        }
+    }
+}
