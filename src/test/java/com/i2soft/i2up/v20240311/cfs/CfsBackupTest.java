7b869e7df52f134d7f6b4ea506311e57ee6628e9
ZHOU Yuan <zhouy2@info2soft.com>
2024-09-23 16:09:37 +0800

    [BF]: CFS新建接口未返回uuid字段

--- src/test/java/com/i2soft/i2up/v20240311/cfs/CfsBackupTest.java
+++ src/test/java/com/i2soft/i2up/v20240311/cfs/CfsBackupTest.java
@@ -42,7 +42,7 @@ public class CfsBackupTest {
         try {
             Response r = auth.client.get(String.format(TestConfig.rapDataUrl, "6601")); // 获取请求数据
             StringMap args = new StringMap().putAll(Objects.requireNonNull(r.jsonToMap())); // 填充请求数据
-            I2Rs.I2SmpRs rs = cfsBackup.createCfsBackup(args); // 发送请求
+            I2Rs.I2CreateRs rs = cfsBackup.createCfsBackup(args); // 发送请求
             Assert.assertNotNull(rs); // 检查结果
         } catch (I2softException e) {
             e.printStackTrace();
