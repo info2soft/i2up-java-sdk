bad8470a84eebc81eeedefe112fb19cd59f0715d
ganl <ganl@info2soft.com>
2025-02-07 15:55:27 +0800

    [TAG]: 9.1.0.4

--- src/main/java/com/i2soft/util/Constants.java
+++ src/main/java/com/i2soft/util/Constants.java
@@ -10,7 +10,7 @@ public final class Constants {
     /**
      * 版本号
      */
-    public static final String VERSION = "9.1.0.3";
+    public static final String VERSION = "9.1.0.4";
     /**
      * 打印 http log
      */
